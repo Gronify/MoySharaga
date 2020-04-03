975c1f101eaf30d167547764d6f6ceedc8542a5c
Gronify <thegronify@gmail.com>
2020-04-03 23:09:39 +0300

    profile page v0.1.2

--- MoyaSharaga/MoyaSharaga/apps/users/views.py
+++ MoyaSharaga/MoyaSharaga/apps/users/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages # django messages for certain actions
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required # decorator for auth
 
+from panel.models import University, Student, Group, Timetable, UserStudent
+
 #view for registration
 def register(request):
     if request.method == 'POST':
@@ -19,4 +21,9 @@ def register(request):
 # view for profile
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    if UserStudent.objects.filter(user = request.user):
+        studentid = UserStudent.objects.filter(user = request.user).values_list('student_id').first()
+        student = Student.objects.get(id = studentid[0])
+        return render(request, 'profile.html', {'student': student})
+    else:
+        return render(request, 'profile.html',)
